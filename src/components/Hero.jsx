0628ec2a96ee7d235aa90159b06f3184ec963b79
Xavier Rice <Xavier.Rice@gmail.com>
2026-08-16 18:23:42 -0400

    fixed spelling in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ function Hero() {
 
       <div className="hero-actions">
         <a className="button primary" href="#listen">
-          Comming Soon!
+          Coming Soon!
         </a>
 
         <a className="button secondary" href="#story">
