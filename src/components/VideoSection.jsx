1921f2d60b47d81a58c5f57b505d0318be279724
Xavier Rice <Xavier.Rice@gmail.com>
2026-08-17 16:31:04 -0400

    fixed watch video

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -13,8 +13,7 @@ function VideoSection()
       </div>
 
       <div className="video-frame">
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/nRuEEylDOoE?si=zkheyJRk3Rpwael8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
-        
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/N81UU7MzLus?si=g1of9QK4MGaAgMNI&amp;start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         
         
               </div>
